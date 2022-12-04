358d7f6162f6f0ca4445c231b8f8e290273b114f
niamhkearney <niamhnichearnaigh@gmail.com>
2022-12-04 16:35:59 +0000

    First commit using Laravel framework.

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,34 @@
+<div>
+    <header>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">WebsiteName</a>
+                <div class="flex-row-reverse collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Hello, [Username]!</a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a class="dropdown-item" href="#">Profile</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Gallery</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="draw_app.php">Draw</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Sign Out</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="text" placeholder="Search Usernames">
+                        <button class="btn btn-primary" type="button">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+</div>
